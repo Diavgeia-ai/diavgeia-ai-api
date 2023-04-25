60f609ce51def7b4ee3b9d03f88bde0c0e4df63b
Christos Porios <christos.porios@gmail.com>
2023-04-25 23:28:03 +0000

    Update db schema

--- sql/db.sql
+++ sql/db.sql
@@ -7,53 +7,49 @@ CREATE TABLE tasks (
   status TEXT NOT NULL,
   params JSONB,
   metrics JSONB,
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
   UNIQUE (type, implementation, name, version),
   UNIQUE (name, version)
 );
 
 CREATE TABLE decisions (
   id SERIAL PRIMARY KEY,
+  ingestor_task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
   ada TEXT NOT NULL,
-  decision_type TEXT NOT NULL,
-  ingestor_name TEXT NOT NULL,
-  ingestor_version INTEGER NOT NULL,
   document_url TEXT NOT NULL,
   metadata JSONB,
-  published_at TIMESTAMP NOT NULL,
-  ingested_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE INDEX idx_ada ON decisions(ada);
-CREATE INDEX idx_decision_type ON decisions(decision_type);
-CREATE INDEX idx_published_at ON decisions(published_at);
+CREATE INDEX idx_created_at ON decisions(created_at);
 
 CREATE TABLE texts (
   id SERIAL PRIMARY KEY,
   decision_id INTEGER REFERENCES decisions(id) ON DELETE CASCADE,
-  text_extractor_name TEXT NOT NULL,
-  text_extractor_version INTEGER NOT NULL,
+  text_extractor_task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
   text TEXT NOT NULL,
-  page_count INTEGER NOT NULL,
-  text_extracted_at TIMESTAMP NOT NULL
+  document_metadata JSONB,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE embeddings (
   id SERIAL PRIMARY KEY,
   text_id INTEGER REFERENCES texts(id) ON DELETE CASCADE,
+  embedder_task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
   embedding_seq INTEGER NOT NULL,
-  embedder_name TEXT NOT NULL,
-  embedder_version INTEGER NOT NULL,
-  embedded_at TIMESTAMP NOT NULL,
-  embedding FLOAT[] NOT NULL
+  embedding FLOAT[] NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE semantic_points (
   id SERIAL PRIMARY KEY,
   decision_id INTEGER REFERENCES decisions(id) ON DELETE CASCADE,
-  reductor_name TEXT NOT NULL,
-  reductor_version INTEGER NOT NULL,
+  reductor_task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
   x FLOAT NOT NULL,
   y FLOAT NOT NULL
 );
